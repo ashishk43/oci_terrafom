0c284537b75d43eceb5f8e0c8ca0c6265d0da504
ashishk43 <97091338+ashishk43@users.noreply.github.com>
2022-04-18 21:14:03 +0530

    mon

--- Compartment/main.tf
+++ Compartment/main.tf
@@ -5,11 +5,11 @@ module "CreateRootCompartment" {
   compartment_variables = var.root_compartment_variables
 }
 
-# locals {
-#   ebs_root_compartment_id = [
-#     for i in module.CreateRootCompartment. : i.id if i.name == "network_compartment"
-#   ]
-# }
+locals {
+  ebs_root_compartment_id = [
+    for i in var.compartment_id : i.id if i.name == "network_compartment"
+  ]
+}
 
 module "CreateCompartmentEBS" {
   source                ="../module/create-compartment"
