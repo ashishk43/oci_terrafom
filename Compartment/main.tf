a8b4edd8d0ba23b65a53525f9a9f83c357433cb9
ashishk43 <97091338+ashishk43@users.noreply.github.com>
2022-04-18 15:36:22 +0530

    mon

--- Compartment/main.tf
+++ Compartment/main.tf
@@ -1,13 +1,13 @@
 // Create Root Compartment
 module "CreateRootCompartment" {
   source                = "../module/create-compartment"
-  parent_ocid           = var.tenancy_ocid
+  parent_ocid           = var.parent_ocid
   compartment_variables = var.root_compartment_variables
 }
 
 locals {
   ebs_root_compartment_id = [
-    for i in module.CreateRootCompartment.compartmentinfo : i.id if i.name == "ebs_compartment"
+    for i in module.CreateRootCompartment : i.id if i.name == "ebs_compartment"
   ]
 }
 
