15c465bd07ffec5c4dc5745e093e0a134fed4b64
ashishk43 <97091338+ashishk43@users.noreply.github.com>
2022-05-13 22:27:02 +0530

    mon

--- IAM/main.tf
+++ IAM/main.tf
@@ -6,6 +6,7 @@ locals {
       for user in group_data.user_list : {
         group_name = group
         user_name = user
+        user_group_map = user+group
       }
     ]  
   ])
@@ -30,7 +31,7 @@ resource "oci_identity_user" "this" {
 
 # Iterate the mapping of users that are members of each group to create the association
 resource "oci_identity_user_group_membership" "test_user_group_membership" {
-    for_each = {for users in local.iam_group_users: users.group_name =>  users}
+    for_each = {for users in local.iam_group_users: users.user_group_map =>  users}
     group_id = oci_identity_group.this[each.value.group_name].id
     user_id = oci_identity_user.this[each.value.user_name].id
 }
