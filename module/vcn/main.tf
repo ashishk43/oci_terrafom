aef7b32e4fd6213ca6d305cc43e4abd45c1ca03b
ashishk43 <97091338+ashishk43@users.noreply.github.com>
2022-05-16 22:50:36 +0530

    mon

--- module/vcn/main.tf
+++ module/vcn/main.tf
@@ -1,6 +1,6 @@
 resource "oci_core_vcn" "this" {
   for_each = var.vcn_config.vcn
-  cidr_blocks    = each.value.vnc_cidr_block
+  cidr_blocks    = [each.value.vnc_cidr_block]
   compartment_id = each.value.compartment_id
   display_name   = each.value.vcn_label_prefix == "none" ? each.value.vcn_display_name : "${each.value.vcn_label_prefix}-${each.value.vcn_display_name}"
   dns_label      = each.value.vcn_dns_label
